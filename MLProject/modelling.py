21a8920c0e9dece117c8b5a8eaed677f45d566a8
davitpp1997 <pratamadavid1997@gmail.com>
2026-08-01 18:51:11 +0700

    update workflow dengan environtment baru

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,30 +1,33 @@
+import os
 import mlflow
 import mlflow.sklearn
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import uuid
 
-# Mengaktifkan MLflow autolog
-mlflow.sklearn.autolog()
+mlflow.sklearn.autolog(log_models=False)
 
-# Memuat data bersih
-df = pd.read_csv('data_tagihan_bersih.csv')
+csv_path = os.environ.get("CSV_URL", "data_tagihan_bersih.csv")
 
-# Memisahkan fitur (X) dan target (y)
-# Kolom 'status_pembayaran' adalah target yang ingin diprediksi
+df = pd.read_csv(csv_path)
+
 X = df.drop(columns=['status_pembayaran'])
 y = df['status_pembayaran']
 
-# Membagi data training dan testing
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 with mlflow.start_run(run_name="Baseline_Model"):
-    # Melatih model Random Forest
     model = RandomForestClassifier(random_state=42)
     model.fit(X_train, y_train)
 
-    # Evaluasi
     predictions = model.predict(X_test)
     acc = accuracy_score(y_test, predictions)
     print(f"Model Accuracy: {acc}")
+
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model",
+        pip_requirements="requirements.txt" 
+    )
